51257e5c10e021d57df7a91a8c0d3217a687c7eb
Topi Pietilänaho <topi.pietilanaho99@gmail.com>
2025-10-29 19:37:51 +0200

    Lopullinen
    Omat funktiot.h ja funktio.cpp

---
+++ h1/H1/funktiot.h
@@ -0,0 +1,11 @@
+#ifndef FUNKTIOT_H
+#define FUNKTIOT_H
+
+void calcSum(int a, int b);
+void calcDiv(float a, float b);
+int retSum(int a, int b);
+float retDiv(float a, float b);
+
+
+
+#endif // FUNKTIOT_H
--- h1/H1/main.cpp
+++ h1/H1/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
+#include "funktiot.h"
 
 using namespace std;
-void calcSum(int a, int b);
-void calcDiv(float a, float b);
-int retSum(int a, int b);
-float retDiv(float a, float b);
+
 
 
 
@@ -37,36 +35,3 @@ int main()
 
 
 
-
-void calcSum(int a, int b){
-    int summa = a+b;
-    cout << "Lukujen " << a << " ja " << b << " summa= " << summa <<endl;
-}
-
-void calcDiv(float a, float b){
-
-    if(b!=0){
-        float osamaara = a/b;
-        cout << "Lukujen " << a << " ja " << b << " osamaara= " << osamaara <<endl;
-    }
-    else
-    {
-        cout << "NOLLALLA EI VOI JAKAA" <<endl;
-    }
-
-}
-
-int retSum(int a, int b){
-    return a+b;
-}
-
-float retDiv(float a, float b)
-{
-    if(b==0){
-        throw runtime_error("jakaja ei saa olla nolla!!!!!.");
-    }else{
-        return a / b;
-    }
-
-
-}
